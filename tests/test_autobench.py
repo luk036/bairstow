ccb8f06660a0afacb566d1264206e81632386d91
Wai-Shing Luk <luk036@gmail.com>
2022-11-15 14:23:06 +0800

    fix regression test

--- tests/test_autobench.py
+++ tests/test_autobench.py
@@ -36,7 +36,7 @@ def test_autocorr(benchmark):
         benchmark ([type]): [description]
     """
     result = benchmark(run_autocorr)
-    assert result <= 7
+    assert result <= 8
 
 
 def test_pbairstow(benchmark):
